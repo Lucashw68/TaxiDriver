7965c7f0618805559d144d2995a3b7111e29224f
Pierre-François <pierre.francois.rissel.pro@gmail.com>
2022-06-17 23:19:18 +0200

    Added Qlearning algorithm

--- cli/src/algorithms/qlearning.py
+++ cli/src/algorithms/qlearning.py
@@ -1,6 +1,14 @@
 import cli.src.gym as gym
 from cli.src.algorithm import Algorithm
+import numpy as np
 
 class Qlearning(Algorithm):
+    def __init__(self, name):
+        super().__init__(name)
+        
+        self.qtable = np.load('./cli/src/algorithms/qtable.npy')
+
     def step(self, env):
-        return env.step(env.action_space.sample())
+        action = np.argmax(self.qtable[env.s])
+        
+        return env.step(action)
